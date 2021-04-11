e607c19dee8ad4c64d1827cd78c50fc1032fde24
escax52 <jucaamaral2000@gmail.com>
2021-04-11 17:59:40 +0100

    Update hubspotController.js

--- BDServer/Controllers/hubspotController.js
+++ BDServer/Controllers/hubspotController.js
@@ -7,6 +7,7 @@ function getClients (req, res) {
     let options = {
         method: "GET",
         url:"https://api.hubapi.com/crm/v3/objects/contacts?hapikey=2f347fca-4639-40c7-af20-c2090d8649b5",
+        headers: {accept: 'application/json'}
     };
 
 request (options, async (error,response,body) => {
@@ -27,9 +28,22 @@ function addClient (req, res) {
     let options = {
         method: "POST",
         url:"https://api.hubapi.com/crm/v3/objects/contacts/search?hapikey=2f347fca-4639-40c7-af20-c2090d8649b5",
-    };
+        headers: {accept: 'application/json', 'content-type': 'application/json'},
+         body: {
+            properties: {
+            company: 'Biglytics',
+            email: 'bcooper@biglytics.net',
+            firstname: 'Bryan',
+            lastname: 'Cooper',
+            phone: '(877) 929-0687',
+            website: 'biglytics.net'
+            }
+         },
+    json: true
+    }
+ };
 
-request (options, async (error,response,body) => {
+request(options, async (error,response,body) => {
     if (error) {
         res.status (400).send({
             message: "Error",
@@ -41,12 +55,13 @@ request (options, async (error,response,body) => {
     }
 });
     
-}
+
 
 function getClientByID (req, res) {
     let options = {
         method: "POST",
         url:"https://api.hubapi.com/crm/v3/objects/contacts/contactId?hapikey=2f347fca-4639-40c7-af20-c2090d8649b5",
+        headers: {accept: 'application/json'}
     };
 
 request (options, async (error,response,body) => {
