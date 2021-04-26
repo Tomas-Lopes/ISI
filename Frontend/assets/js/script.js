d1a09da1a23cbdce7a7184075281732faa40a1a9
beatrizazeved03 <biaazevedo2000@gmail.com>
2021-04-26 17:52:18 +0100

    Front atualizado

---
+++ Frontend/assets/js/script.js
@@ -0,0 +1,30 @@
+function viewPassword()
+{
+  var passwordInput = document.getElementById('password-field');
+  var passStatus = document.getElementById('pass-status');
+ 
+  if (passwordInput.type == 'password'){
+    passwordInput.type='text';
+    passStatus.className='fa fa-eye-slash';
+    
+  }
+  else{
+    passwordInput.type='password';
+    passStatus.className='fa fa-eye';
+  }
+}
+function confirmPassword()
+{
+  var passwordInput = document.getElementById('password-confirm');
+  var passStatus = document.getElementById('pass-confirm');
+ 
+  if (passwordInput.type == 'password'){
+    passwordInput.type='text';
+    passStatus.className='fa fa-eye-slash';
+    
+  }
+  else{
+    passwordInput.type='password';
+    passStatus.className='fa fa-eye';
+  }
+}
