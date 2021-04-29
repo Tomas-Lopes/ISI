105dec6764ce5abd2783700af4431a93e216c478
CarolinaAlves <a89259@alunos.uminho.pt>
2021-04-29 19:01:24 +0100

    another one

--- Frontend/controllers/projeto.controller.js
+++ Frontend/controllers/projeto.controller.js
@@ -1,11 +1,12 @@
+window.onload = function () {
 const formNovoPedido = document.getElementById("formNovoPedido");
 
-function submeterPedido() {
-
-    formNovoProjeto.addEventListener('submit', (event) => {
-        document.getElementById("register").disabled = true;
+    
+    formNovoPedido.addEventListener('submit', (event) => {
         event.preventDefault();
-
+        document.getElementById("register").disabled = true;
+        
+        console.log("tou");
         //localizacao com api
         var tipoInput = document.getSelection("tipoProjeto").value;
         var descricaoInput = document.getElementById("description").value;
@@ -27,66 +28,42 @@ function submeterPedido() {
             mode: 'cors',
             body: JSON.stringify(data)
         }).then(response => {
-
+            console.log(response);
             if (response.status == 200) {
-                document.getElementById('pedidocriado').click();
-                setTimeout(function () {
-                    window.location.replace('./cliente.html');
-                }, 2000);
-            } else {
-                document.getElementById('pedidonaocriado').click();
-                setTimeout(function () {
-                    location.reload();
-                }, 2000);
-            }
-            return result.json();
-        }).then(data => {
-            console.log(data);
-        })
-    })
-}
-/*
- if (result.message == "Projeto criado com sucesso") {
-                swal({
-                    title: 'Pedido de projeto efetuado com sucesso!',
+                Swal.fire({
+                    title: 'Pedido inserido com sucesso!',
                     type: 'success',
                     showCancelButton: false,
                     showConfirmButton: false,
                     showLoaderOnConfirm: false,
                     timer: 2000
-                }).then(result => {
-                    window.location.replace('/cliente.html')
+                }).then(response => {
+                    
+                    window.location.replace('./cliente.html')
                 })
-            } else {
 
-                if (result.error == "PROJECT_EXISTS") {
-                    throw new Error(result.error);
+                //document.getElementById('pedidocriado').click();
+                /*setTimeout(function () {
+                    window.location.replace('./cliente.html');
+                }, 2000);*/
+            } else {
+                if (response.error == "Tenta outra vez") {
+                    throw new Error(response.error);
                 } else {
                     throw new Error(
-                        "Este Projeto já existe, tente novamente!"
+                        "Tenta outra vez"
                     )
-                }
+                //document.getElementById('pedidonaocriado').click();
+                /*setTimeout(function () {
+                    location.reload();
+                }, 2000);*/
             }
+            return response.json();
         }).catch(error => {
-            document.getElementById("login").disabled = false;
-            if (error.message == 'CONTACT_EXISTS') {
-                swal({
-                    title: 'Este email já existe!',
-                    showCancelButton: false,
-                    showConfirmButton: false,
-                    type: 'error',
-                    timer: 2000
-                })
-            } else {
-                swal({
-                    html: '<strong><h3>Ocorreu um erro! Tente novamente mais tarde. Obrigado!</h3></strong>',
-                    showCancelButton: false,
-                    showConfirmButton: false,
-                    type: 'error',
-                    timer: 2000
-                })
-            }
-        });
-    });
+            console.log(error);
+        })
+    })
 }
-*/
+
+
+
