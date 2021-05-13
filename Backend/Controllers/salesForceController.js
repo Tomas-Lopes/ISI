9a7af62d20e1c14fbdc9f0ef19d0744ab85915b7
Tomas-Lopes <56835128+Tomas-Lopes@users.noreply.github.com>
2021-05-13 19:08:43 +0100

    .

---
+++ Backend/Controllers/salesForceController.js
@@ -0,0 +1,25 @@
+const request = require("request");
+const bcrypt = require("bcrypt");
+const hubspot = require("./hubspotController");
+
+function projectSalesForce(req, res) {
+    const amount = req.body.amount;
+    const closedate = req.body.closedate;
+    const dealname = req.body.dealname;
+    const dealstage = req.body.dealstage;
+    const description = req.body.description;
+    const project_type = req.body.project_type;
+  
+    const properties = {
+        Amount__c: amount,
+        Closedate__c: closedate,
+        Dealname__c: dealname,
+        Dealstage__c: dealstage,
+        Description__c: description,
+        TipoProjeto__c: project_type,
+      hubspot_owner_id: "69176641",
+      pipeline: "default",
+      Arq_Id__c: "0",
+      Gestor_Id__c: "1",
+    };
+  
