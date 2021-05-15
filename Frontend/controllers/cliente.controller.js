198092ceb990f755e85885d106ea09eb25b95cc6
escax52 <jucaamaral2000@gmail.com>
2021-05-15 22:11:11 +0100

    lista clientes

---
+++ Frontend/controllers/cliente.controller.js
@@ -0,0 +1,36 @@
+//Fetch Headers
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+//Fetch requestOptions
+const requestOptions = {
+    mode: 'cors',
+    method: 'GET',
+    headers: myHeaders,
+    credentials: 'include'
+};
+
+//carregar pedidos
+getClientes();
+
+async function getClientes() {
+
+    try {
+        let conteudo = "";
+        //Fetch
+        const response = await fetch(`http://127.0.0.1:8080/user/clients`, requestOptions);
+        const clientes = await response.json();
+        console.log(clientes);
+        for (const clientes of clientes.results) {
+            conteudo += "<tr><td> " + clientes.vid + "</td>";
+            conteudo += "<td> " + clientes.properties.firstname + "</td>";
+            conteudo += "<td> " + clientes.properties.email + "</td>";
+            conteudo += "<td> " + clientes.properties.nif + "</td>";
+            conteudo += "<td> " + clientes.properties.phone + "</td>";
+        }
+
+        document.getElementById("bodyClientes").innerHTML = conteudo;
+
+    } catch (error) {
+        console.log("Erro:" + error);
+    }
+}
