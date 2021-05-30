ed023ec4df0c5e6128b475be4e9d186a811e9ca3
beatrizazeved03 <biaazevedo2000@gmail.com>
2021-05-30 01:45:39 +0100

    50/50 dos pontos no gis

--- Frontend/controllers/arquiteto.controller.js
+++ Frontend/controllers/arquiteto.controller.js
@@ -1,5 +1,5 @@
 //window.onload = function () {
-console.log("ola nuno");
+//console.log("ola nuno");
 //const formAssociar = document.getElementById("formAssociar");
 
 /*formAssociar.addEventListener('submit', (event) => {
@@ -96,9 +96,11 @@ async function pedidosArquiteto() {
         let conteudo = "";
         const response = await fetch(`http://127.0.0.1:8080/user/projetosArquiteto`, requestOptions);
         const pedidos = await response.json();
+
+        
         _pedidos = pedidos;
         console.log(pedidos);
-        pedidos.forEach( (pedido, index) => {
+        pedidos.forEach((pedido, index) => {
             conteudo += "<tr><td> " + pedido.Dealname__c + "</td>";
             conteudo += "<td> " + pedido.Name + "</td>";
             conteudo += "<td> " + pedido.Localizacao__c + "</td>";
@@ -107,21 +109,89 @@ async function pedidosArquiteto() {
             conteudo += "<td> " + pedido.Amount__c + "</td>";
             conteudo += "<td> " + getDate(pedido.Closedate__c) + "</td>";
             conteudo += "<td> " + pedido.Dealstage__c + "</td>";
-            conteudo += '<td> <button onclick="openModal('+ index +')" data-backdrop="false" type="button" id="modal"  style=" padding: 15px; border-radius: 50%;margin-left: 07px;" class="btn" ><i class="fas fa-info-circle"></i></button>' + "</td></tr>";
+            conteudo += '<td> <button onclick="openModal(' + index + ')" data-backdrop="false" type="button" id="modal"  style=" padding: 15px; border-radius: 50%;margin-left: 07px;" class="btn" ><i class="fas fa-info-circle"></i></button>' + "</td></tr>";
         
-        });
+        
+            require([
+                "esri/config",
+                "esri/Map",
+                "esri/views/MapView",
 
+                "esri/widgets/BasemapToggle",
+                "esri/widgets/BasemapGallery",
+
+                "esri/Graphic",
+                "esri/layers/GraphicsLayer"
+
+            ], function (esriConfig, Map, MapView, BasemapToggle, BasemapGallery, Graphic, GraphicsLayer) {
+                console.log("teste")
+                esriConfig.apiKey = "AAPK4794dac93f2f4bfb9d5c5bc14c437164XR9J186t8cbfv7LaCox9yDhOY2MhlkPIatcbgP88-kkUORDpAZr1jen-wKBkGAhN";
+
+                const map = new Map({
+                    basemap: "arcgis-topographic" //Basemap layer service
+                });
+
+                const view = new MapView({
+                    map: map,
+                    center: [-8.293912, 41.450346], //Longitude, latitude
+                    zoom: 11,
+                    container: "viewDiv"
+                });
+
+                const basemapToggle = new BasemapToggle({
+                    view: view,
+                    nextBasemap: "arcgis-imagery"
+                });
+
+                view.ui.add(basemapToggle, "bottom-right");
+
+                const basemapGallery = new BasemapGallery({
+                    view: view,
+                    source: {
+                        query: {
+                            title: '"World Basemaps for Developers" AND owner:esri'
+                        }
+                    }
+                });
+
+                const graphicsLayer = new GraphicsLayer();
+                map.add(graphicsLayer);
+
+                const simpleMarkerSymbol = {
+                    type: "simple-marker",
+                    color: [226, 119, 40],  // Orange
+                    outline: {
+                        color: [255, 255, 255], // White
+                        width: 1
+                    }
+                };
+
+                const point = {
+                    type: "point",
+                    longitude: pedido.Longitude__c,
+                    latitude: pedido.Latitude__c
+                };
+                //ponto azurem
+                const pointGraphic = new Graphic({
+                    geometry: point,
+                    symbol: simpleMarkerSymbol
+                });
+                graphicsLayer.add(pointGraphic);
+            })
+        
+        
+    });
 
         document.getElementById("bodyArquiteto").innerHTML = conteudo;
-      
+
 
     } catch (error) {
         console.log("Erro:" + error);
     }
- 
+
 }
 
-function openModal (index) {
+function openModal(index) {
     $("#myModal").modal('show');
     $("#Dealname__c").val(_pedidos[index].Dealname__c);
     $("#Localizacao__c").val(_pedidos[index].Localizacao__c);
